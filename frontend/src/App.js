1d6c80c2e08ee9f7be8817ae149d4ccf1cf7ca53
AdrienBlanco <adrienblanco89@gmail.com>
2023-12-17 21:18:09 +0100

    Mise en place de Redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from 'react-redux';
 import Home from "./pages/Home/Home";
 import SignIn from "./pages/SignIn/SignIn";
 import User from "./pages/User/User";
 
 function App() {
+
+  const token = useSelector((state) => state.auth.token);
+
   return (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/user" element={<User />} />
+        <Route path="/user" element={token ? <User /> : <Navigate to="/sign-in" />} />
       </Routes>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import store from "./redux/store";
+import { Provider } from 'react-redux';
 import Header from './layout/Header/Header';
 import App from './App';
 import Footer from './layout/Footer/Footer';
@@ -8,11 +10,13 @@ import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Header />
-    <App />
-    <Footer />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Header />
+      <App />
+      <Footer />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
