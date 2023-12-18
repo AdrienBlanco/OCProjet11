289bb35d50ad3ac9e31d0b617e844fa514196be6
AdrienBlanco <adrienblanco89@gmail.com>
2023-12-18 05:19:09 +0100

    relecture

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,13 +10,15 @@ import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Header />
-      <App />
-      <Footer />
-    </BrowserRouter>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <App />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
